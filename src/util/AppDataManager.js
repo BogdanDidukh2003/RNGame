35dd0227a17973730938bbb14ad8d3b8b3040b12
Denys Shutka <nden2105@gmail.com>
2021-01-19 18:22:50 +0200

    fix: correct colors of status bar

--- src/util/AppDataManager.js
+++ src/util/AppDataManager.js
@@ -1,21 +1,43 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import { StatusBar, useColorScheme } from 'react-native';
 
 import CONSTANTS from './../constants';
 
 const AppDataContext = createContext();
 
+const configureStatusBar = (theme) => {
+  StatusBar.setBarStyle(
+    (theme == CONSTANTS.THEMES.LIGHT
+      ? 'dark' : 'light'
+    ) + '-content');
+  StatusBar.setBackgroundColor(
+    theme == CONSTANTS.THEMES.LIGHT
+      ? CONSTANTS.COLORS.MAIN_BACKGROUND
+      : CONSTANTS.COLORS.DARK_BACKGROUND
+  );
+};
+
 const AppDataProvider = ({ children }) => {
   const [theme, setTheme] = useState(useColorScheme() || CONSTANTS.THEMES.LIGHT);
 
+  useEffect(() => {
+    configureStatusBar(theme);
+  });
+
+  const changeThemeTo = (theme) => {
+    setTheme(theme);
+    configureStatusBar(theme);
+  };
+
   const switchTheme = () => {
+    let newTheme;
     if (theme == CONSTANTS.THEMES.LIGHT) {
-      setTheme(CONSTANTS.THEMES.DARK);
-      StatusBar.setBarStyle('light-content');
+      newTheme = CONSTANTS.THEMES.DARK;
     } else {
-      setTheme(CONSTANTS.THEMES.LIGHT);
-      StatusBar.setBarStyle('dark-content');
+      newTheme = CONSTANTS.THEMES.LIGHT;
     }
+
+    changeThemeTo(newTheme);
   };
 
   return (
